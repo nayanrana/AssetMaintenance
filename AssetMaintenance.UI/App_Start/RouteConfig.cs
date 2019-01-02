a67527d9ee34fd89dc1835e114b46332451d3b87
nayan <nayan@meandmytech.com>
2019-01-02 19:12:24 +0530

    validate maintenance type.

--- AssetMaintenance.UI/App_Start/RouteConfig.cs
+++ AssetMaintenance.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AssetMaintenance.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Maintenance", action = "MaintenanceByStatus", id = UrlParameter.Optional }
             );
         }
     }
